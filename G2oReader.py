0a45662ee91b9f48f351bb53e48723115956ba41
jiangpin-legend <3180105573@zju.edu.cn>
2022-12-08 17:02:28 +0800

    spread_separator tested

--- G2oReader.py
+++ G2oReader.py
@@ -1,36 +1,77 @@
 #!/usr/bin/python3.6
 # -*- coding: UTF-8 -*-
 
-def edge_dict2list():
-    pass
-def vertex_dict2list():
-    pass
-
 class G2oReader:
     def __init__(self) -> None:
         self.vertex = {}
         self.edge = {}
     
     def read(self,file_name):
-        self.file_name = file_name
-        self.data = open(file_name,'r')
-        for each_line in self.data:
-            try:
-                g2o__line = each_line.split(' ')
-                identifier= g2o__line[0]
-                if identifier == 'VERTEX_SE3:QUAT':
-                    self.vertex[g2o__line[1]] = (g2o__line[2:-1])
-                elif identifier=='EDGE_SE3:QUAT':
-                    self.edge[(g2o__line[1],g2o__line[2])] = (g2o__line[3:-1])
-            except ValueError:
-                pass
-        self.data.close()
+        with open(file_name,'r') as g2o_file:
+            for each_line in g2o_file:
+                try:
+                    g2o__line = each_line.split(' ')
+                    identifier= g2o__line[0]
+                    if identifier == 'VERTEX_SE3:QUAT':
+                        self.vertex[g2o__line[1]] = (g2o__line[2:-1])
+                    elif identifier=='EDGE_SE3:QUAT':
+                        self.edge[(g2o__line[1],g2o__line[2])] = (g2o__line[3:-1])
+                except ValueError:
+                    pass
+        
         return self.vertex,self.edge
   
+    def write(self,file_name,vertex_lines,edge_lines):
+        with open(file_name,'w') as g2o_file:
+            g2o_file.writelines(vertex_lines)
+            g2o_file.writelines(edge_lines) 
+
+    def write_dict(self,file_name,vertex_dict,edge_dict):
+        vertex_lines = self.vertex_dict2lines(vertex_dict)
+        edge_lines = self.edge_dict2lines(edge_dict)
+        self.write(file_name,vertex_lines,edge_lines)
+
+    def vertex_dict2list(self,vertex_dict):
+        vertex_list = []
+        for key,data in zip(vertex_dict.keys(),vertex_dict.values()):
+            vertex_list.append(key)
+            for each in data:
+                vertex_list.append(each)
+        return vertex_list
+
+    def edge_dict2list(self,edge_dict):
+        edge_list = []
+        for key_pair,data in zip(edge_dict.keys(),edge_dict.values()):
+            edge_list.append(key_pair[0])
+            edge_list.append(key_pair[1])
+            for each in data:
+                edge_list.append(each)
+        return edge_list
+    
+    def vertex_dict2lines(self,vertex_dict):
+        vertex_lines = []
+        for key,data in zip(vertex_dict.keys(),vertex_dict.values()):
+            line = ''
+            line =line+key
+            for each in data:
+                line = line+' '+each
+            line += '\n'
+            vertex_lines.append(line)
+        return vertex_lines
+
+    def edge_dict2lines(self,edge_dict):
+        edge_lines = []
+        for key_pair,data in zip(edge_dict.keys(),edge_dict.values()):
+            edge_line = key_pair[0]+' '+key_pair[1]
+            for each in data:
+                edge_line = edge_line+' '+each
+            edge_line += '\n'
+            edge_lines.append(edge_line)
+        return edge_lines
 
 if __name__ == '__main__':
     g2o_reader = G2oReader()
     vertex,edge = g2o_reader.read('/home/jiangpin/dataset/example_4robots/0.g2o')
-    print(vertex)
+    # print(vertex)
     print()
     print(edge)
--- multi_robot_tools.py
+++ multi_robot_tools.py
@@ -1,4 +1,8 @@
+#!/usr/bin/python3.6
+# -*- coding: UTF-8 -*-
+
 import os
+import copy
 
 from G2oReader import G2oReader
 
@@ -7,8 +11,8 @@ class Separator():
         self.vertex = {}
         self.edge = {}
         
-    def exisit(self,key_pair):
-        for sep_key_pair in self.edge.keys:
+    def exists(self,key_pair):
+        for sep_key_pair in self.edge.keys():
             if(sep_key_pair[0] == key_pair[0] and sep_key_pair[1]==key_pair[1]):
                 return True
             if(sep_key_pair[0] ==key_pair[1] and sep_key_pair[1]==key_pair[0]):
@@ -27,17 +31,22 @@ class MultiRobotTools():
         self.robot_num = robot_num
         self.vertex_dict = {}
         self.edge_dict = {}
+        #sum graph
+        self.vertex_dict_sum = {}
+        self.edge_dict_sum = {}
+        #seprarator
         self.separator = Separator()
+        self.g2o_reader = G2oReader()
     
     def read_g2o(self):
         for robot_id in range(self.robot_num):
             file_name = os.path.join(self.data_dir,str(robot_id)+'.g2o')
-            g2o_reader = G2oReader(file_name)
-            vertex,edge = g2o_reader.read()
+            vertex,edge = self.g2o_reader.read(file_name)
+            # print(edge)
             self.vertex_dict[robot_id] = vertex
             self.edge_dict[robot_id] = edge
-    
-    def key2_robot_id(self,key):
+
+    def key2robot_id(self,key):
         robot_id = int(key)//100000000000000000
         robot_id -= 69
         return robot_id
@@ -45,10 +54,9 @@ class MultiRobotTools():
     def is_separator(self,key_pair):
         key0 = key_pair[0]
         key1 = key_pair[1]
-        id0  = self.key2_robot_id(key0)
-        id1  = self.key2_robot_id(key1)
+        id0  = self.key2robot_id(key0)
+        id1  = self.key2robot_id(key1)
         return id0 != id1
-
 
     def rename_gtsam_id(self):
         #rename the id in gtsam so that g2o can handle
@@ -57,26 +65,67 @@ class MultiRobotTools():
     
     def aggregate_graph(self):
         #aggregate graph from each g2o file into one g2o file
-        pass
+        for robot_id in range(self.robot_num):
+            vertex_dict = self.vertex_dict[robot_id]
+            edge_dict = self.edge_dict[robot_id]
 
-    def partition_graph(self):
-        #partition an aggregate graph
-        pass
+            self.vertex_dict_sum.update(vertex_dict)
+            self.edge_dict_sum.update(edge_dict)
+        file_name = os.path.join(self.data_dir,'full_graph'+'.g2o')
+        self.g2o_reader.write_dict(file_name,vertex_dict,edge_dict)
+
+
+    def partition_graph(self,file_name):
+        vertex,edge = self.g2o_reader.read(file_name)
+        base_name = file_name.split('.')
+
+        vertex_dict = {}
+        edge_dict = {}
+        
+        for robot_id in range(self.robot_num):
+            for key in vertex.keys():
+                if(self.key2robot_id(key)==robot_id):
+                    vertex_dict[robot_id].update({key,vertex[key]})
+            for key_pair in edge.keys():
+                if(self.key2robot_id(key_pair[0])==robot_id or self.key2robot_id(key_pair[1])==robot_id):
+                    edge_dict[robot_id].update({key_pair,edge[key_pair]})
+            self.g2o_reader.write_dict(base_name+'_'+str(robot_id)+'.g2o',vertex_dict[robot_id],edge_dict[robot_id])
+        
     
     def aggregate_separator(self):
-        for each_robot in self.edge_dict.keys:
+        for each_robot in self.edge_dict.keys():
             edge_dict = self.edge_dict[each_robot]
             vertex_dict = self.vertex_dict[each_robot]
-            for key_pair in edge_dict.keys:
+            for key_pair in edge_dict.keys():
+                print(key_pair)
                 if(self.is_separator(key_pair)):
-                    if(not self.separator.exsist(key_pair)):
+                    if(not self.separator.exists(key_pair)):
                         vertex0 = vertex_dict[key_pair[0]]
                         vertex1 = vertex_dict[key_pair[1]]
                         edge = edge_dict[key_pair]
                         self.separator.add(key_pair,vertex0,vertex1,edge)
-
+        
 
     def spread_separator(self):
         #spread separator among robot for dist-optimization
-        pass
+        self.aggregate_separator()
+        edge_dict_copy = copy.deepcopy(self.edge_dict)
+        vertex_dict_copy = copy.deepcopy(self.vertex_dict)
+        
+        for robot_id in edge_dict_copy.keys():
+            edge_dict = edge_dict_copy[robot_id]
+            vertex_dict = vertex_dict_copy[robot_id]
+            # print(self.separator.edge.keys())
+            edge_dict.update(self.separator.edge)
+            vertex_dict.update(self.separator.vertex)
+            file_name = os.path.join(self.data_dir,str(robot_id)+'_separator'+'.g2o')
+            self.g2o_reader.write_dict(file_name,vertex_dict,edge_dict)
+        
+
+if __name__ == '__main__':
+    data_dir = '/home/jiangpin/dataset/test_4robots'
+    num = 4
+    multi_robot_tools = MultiRobotTools(data_dir,num)
+    multi_robot_tools.read_g2o()
     
+    multi_robot_tools.spread_separator()
