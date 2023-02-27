dd07204e926a77980a3e8b9ad5481e90690e0ede
jiangpin-legend <3180105573@zju.edu.cn>
2023-02-27 23:42:43 +0800

    Add partition_graph_g2o

--- partition_graph.py
+++ partition_graph.py
@@ -9,7 +9,13 @@ if __name__ =="__main__":
   filename = "/home/jiangpin/dataset/2yuan_new/full_graph.g2o"
   data_dir = "/home/jiangpin/dataset/2yuan_new/"
   num = 2
+  
+  filename = "/home/jiangpin/dataset/2yuan_test/full_graph.g2o"
+  data_dir = "/home/jiangpin/dataset/2yuan_test/"
+
+
   multi_robot_tools = MultiRobotTools(data_dir,num)
-  multi_robot_tools.partition_graph(filename)
+  # multi_robot_tools.read_g2o()
+  multi_robot_tools.partition_graph_g2o(filename)
 
 
